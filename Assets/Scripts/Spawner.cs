f5e4999c7a287d2ccc7b8c95c9ae032f692d376e
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-02-11 11:44:49 -0600

    Part 3 Part 1

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject bombPrefab;
+    public float delay = 2f;
+    public bool active = false;
+    public Vector2 delayRange = new Vector2(2, 1);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
