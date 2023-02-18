bfcc6d4fc15299c84245a399ff7445a6eb91194b
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-02-18 11:41:17 -0600

    Stuff

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,15 +6,38 @@ public class Spawner : MonoBehaviour
 {
     public GameObject bombPrefab;
     public float delay = 2f;
-    public bool active = false;
-    public Vector2 delayRange = new Vector2(2, 1);
+    public bool active = true;
+    public Vector2 delayRange = new Vector2(1, 2);
+    private Vector2 screenBounds;
+    private float objectHeight;
+    private float objectLength;
     // Start is called before the first frame update
     void Start()
     {
         ResetDelay();
         StartCoroutine(EnemyGenerator());
-    }
 
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectLength = bombPrefab.GetComponent<MeshRenderer>().bounds.size.x / 2f;
+        objectHeight = bombPrefab.GetComponent<MeshRenderer>().bounds.size.y / 2f;
+    }
+    IEnumerator EnemyGenerator()
+    {
+        yield return new WaitForSeconds(delay);
+        if (active)
+        {
+            float randomX = Random.Range(screenBounds.x - objectLength, screenBounds.x * -1 + objectLength);
+            float spawnY = (screenBounds.y + objectHeight) + 5;
+
+            Instantiate(bombPrefab, new Vector3(randomX, spawnY, 0), bombPrefab.transform.rotation);
+            ResetDelay();
+        }
+        StartCoroutine(EnemyGenerator());
+    }
+    void ResetDelay()
+    {
+        delay = Random.Range(delayRange.x, delayRange.y);
+    }
     // Update is called once per frame
     void Update()
     {
