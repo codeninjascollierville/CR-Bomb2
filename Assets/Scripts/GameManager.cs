60f4c55b561ab4edd6cc61f470359089520e3eb0
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-02-25 12:07:57 -0600

    E

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private Spawner spawner;
+    public GameObject title;
+    public float score = 0;
+    private Vector2 screenBounds;
+    private GameObject playerPrefab;
+    private GameObject player;
+    private bool gameStarted = false;
 
     private void Awake()
     {
         spawner = GameObject.Find("Spawner").GetComponent<Spawner>();
+        player = playerPrefab;
     }
     // Start is called before the first frame update
     void Start()
@@ -19,9 +26,30 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.anyKeyDown)
+        if(!gameStarted)
         {
-            spawner.active = true;
+            if (Input.anyKeyDown)
+            {
+                resetGame();
+            }
+        }
+        var nextBomb = GameObject.FindGameObjectsWithTag("Bomb");
+        foreach (GameObject bombObject in nextBomb)
+        {
+            if (bombObject.transform.position.y < (-screenBounds.y)-12)
+            {
+                Destroy(bombObject);
+            }
         }
     }
+    void scoreUp()
+    {
+        score++;
+    }
+    void resetGame()
+    {
+        spawner.active = true;
+        title.SetActive(false);
+    }
+    
 }
